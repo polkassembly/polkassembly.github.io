3de9083c1188a31a29f512d93fd49a9314e9b9c4
alphainfinitus <kumar.mridul000@gmail.com>
2023-05-30 14:55:36 +0530

    setup gh-pages and deployment, unbuilt code to main

---
+++ src/components/parachainsArr.ts
@@ -0,0 +1,242 @@
+import polkadotLogo from '../assets/parachain-logos/polkadot-logo.jpg'
+import kusamaLogo from '../assets/parachain-logos/kusama-logo.gif'
+import moonriverLogo from '../assets/parachain-logos/moonriver-logo.png'
+import moonbeamLogo from '../assets/parachain-logos/moonbeam-logo.png'
+import acalaLogo from '../assets/parachain-logos/acala-logo.jpg'
+import acuityLogo from '../assets/parachain-logos/acuity-logo.jpg'
+import altairLogo from '../assets/parachain-logos/altair-logo.jpeg'
+import amplitudeLogo from '../assets/parachain-logos/amplitude-logo.png'
+import astarLogo from '../assets/parachain-logos/astar-logo.png'
+import automataLogo from '../assets/parachain-logos/automata-logo.jpg'
+import basiliskLogo from '../assets/parachain-logos/basilisk-logo.jpg'
+import calamariLogo from '../assets/parachain-logos/calamari-logo.png'
+import centrifugeLogo from '../assets/parachain-logos/centrifuge-logo.png'
+import gearLogo from '../assets/parachain-logos/gear-logo.jpg'
+import hydradxLogo from '../assets/parachain-logos/hydradx-logo.jpg'
+import karuraLogo from '../assets/parachain-logos/karura-logo.jpg'
+import khalaLogo from '../assets/parachain-logos/khala-logo.png'
+import kiltLogo from '../assets/parachain-logos/kilt-logo.png'
+import kylinLogo from '../assets/parachain-logos/kylin-logo.png'
+import mantaLogo from '../assets/parachain-logos/manta-logo.jpg'
+import moonbaseLogo from '../assets/parachain-logos/moonbase-logo.png'
+import parallelLogo from '../assets/parachain-logos/parallel-logo.jpg'
+import pendulumLogo from '../assets/parachain-logos/pendulum-logo.png'
+import pioneerLogo from '../assets/parachain-logos/bitcountrypioneer-logo.jpg'
+import polkadexLogo from '../assets/parachain-logos/polkadex-logo.jpg'
+import robonomicsLogo from '../assets/parachain-logos/robonomics-logo.jpg'
+import shibuyaLogo from '../assets/parachain-logos/shiden-logo.jpg'
+import shidenLogo from '../assets/parachain-logos/shiden-logo.jpg'
+// import tanganikaLogo from '../assets/parachain-logos/tanganika-logo.png'
+import tinkerLogo from '../assets/parachain-logos/chain-logo.jpg'
+import turingLogo from '../assets/parachain-logos/turing-logo.png'
+import westendLogo from '../assets/parachain-logos/westend-logo.jpg'
+import composableLogo from '../assets/parachain-logos/composable-finance-logo.png'
+import crustLogo from '../assets/parachain-logos/crust-logo.svg'
+import equilibriumLogo from '../assets/parachain-logos/equilibrium.png'
+import frequencyLogo from '../assets/parachain-logos/frequency-logo.png'
+import gmordieLogo from '../assets/parachain-logos/gmordie-logo.png'
+import genshiroLogo from '../assets/parachain-logos/genshiro.png'
+import picassoLogo from '../assets/parachain-logos/picasso-logo.svg'
+import varaLogo from '../assets/parachain-logos/vara-logo.svg'
+import xxcoinLogo from '../assets/parachain-logos/xxcoin-logo.svg'
+import pichiuLogo from '../assets/parachain-logos/pichiu-logo.png'
+import snowLogo from '../assets/parachain-logos/snow-logo.png'
+import polymeshLogo from '../assets/parachain-logos/polymesh-logo.png'
+import tidechainLogo from '../assets/parachain-logos/tidechain-logo.svg'
+
+const parachainsArr: {title:string, image:string}[] = [
+	{
+		image: polkadotLogo,
+		title: 'polkadot'
+	},
+	{
+		image: kusamaLogo,
+		title: 'kusama'
+	},
+	{
+		image: moonriverLogo,
+		title: 'moonriver'
+	},
+	{
+		image: moonbeamLogo,
+		title: 'moonbeam'
+	},
+	{
+		image: acalaLogo,
+		title: 'ACALA'
+	},
+	{
+		image: acuityLogo,
+		title: 'acuity'
+	},
+	{
+		image: altairLogo,
+		title: 'altair'
+	},
+	{
+		image: amplitudeLogo,
+		title: 'amplitude'
+	},
+	{
+		image: astarLogo,
+		title: 'astar'
+	},
+	{
+		image: automataLogo,
+		title: 'automata'
+	},
+	{
+		image: basiliskLogo,
+		title: 'basilisk'
+	},
+	{
+		image: calamariLogo,
+		title: 'calamari'
+	},
+	{
+		image: centrifugeLogo,
+		title: 'centrifuge'
+	},
+	{
+		image: composableLogo,
+		title: 'composable'
+	},
+	{
+		image: crustLogo,
+		title: 'crust'
+	},
+	{
+		image: crustLogo,
+		title: 'crustShadow'
+	},
+	{
+		image: gearLogo,
+		title: 'gear'
+	},
+	{
+		image: parallelLogo,
+		title: 'heiko'
+	},
+	{
+		image: hydradxLogo,
+		title: 'hydraDX'
+	},
+	{
+		image: karuraLogo,
+		title: 'karura'
+	},
+	{
+		image: khalaLogo,
+		title: 'khala'
+	},
+	{
+		image: kiltLogo,
+		title: 'kilt'
+	},
+	{
+		image: kylinLogo,
+		title: 'kylin'
+	},
+	{
+		image: mantaLogo,
+		title: 'manta'
+	},
+	{
+		image: moonbaseLogo,
+		title: 'moonbase'
+	},
+	{
+		image: parallelLogo,
+		title: 'parallel'
+	},
+	{
+		image: pendulumLogo,
+		title: 'pendulum'
+	},
+	{
+		image: pioneerLogo,
+		title: 'pioneer'
+	},
+	{
+		image: polkadexLogo,
+		title: 'polkadex'
+	},
+	{
+		image: robonomicsLogo,
+		title: 'robonomics'
+	},
+	{
+		image: shibuyaLogo,
+		title: 'shibuya'
+	},
+	{
+		image: shidenLogo,
+		title: 'shiden'
+	},
+	// {
+	// 	image: tanganikaLogo,
+	// 	title: 'tanganika'
+	// },
+	{
+		image: tinkerLogo,
+		title: 'tinker'
+	},
+	{
+		image: turingLogo,
+		title: 'turing'
+	},
+	{
+		image: westendLogo,
+		title: 'westend'
+	},
+	{
+		image: equilibriumLogo,
+		title: 'equilibrium'
+	},
+	{
+		image: frequencyLogo,
+		title: 'frequency-rococo'
+	},
+	{
+		image: genshiroLogo,
+		title: 'genshiro'
+	},
+	{
+		image: gmordieLogo,
+		title: 'gmordie'
+	},
+	{
+		image: picassoLogo,
+		title: 'picasso'
+	},
+	{
+		image: pichiuLogo,
+		title: 'pichiu'
+	},
+	{
+		image: kylinLogo,
+		title: 'pichiu-rococo'
+	},
+	{
+		image: polymeshLogo,
+		title: 'polymesh'
+	},
+	{
+		image: snowLogo,
+		title: 'snow'
+	},
+	{
+		image: tidechainLogo,
+		title: 'tidechain'
+	},
+	{
+		image: varaLogo,
+		title: 'vara'
+	},
+	{
+		image: xxcoinLogo,
+		title: 'Xx'
+	},
+];
+
+export default parachainsArr;
